9be97ffab310bce284fe589dd980098110d0bf61
Francisco Villafáñez <110670253+FranVillafanez@users.noreply.github.com>
2022-09-14 12:41:11 -0300

    ajustes funcionamiento

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Plantilla para crear un usuario
 class Usuario {
-    constructor(nombre, apellido, cargo) {
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.cargo = cargo
-    }
+  constructor(nombre, apellido, cargo) {
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.cargo = cargo
+  }
 }
 // Arreglo para registrar un usuario y fecha
 let registros = []
@@ -13,23 +13,24 @@ const peopleTable = document.querySelector ('#peopleTable tbody');
 const dataForm = document.querySelector ("#addPeople");
 
 function saveData (){
-  const newRegistro = new Usuario (
-    dataForm.camponNombre.value, 
-    dataForm.camponApellido.value,
-    dataForm.selector.value
-  );
-  registros.push (newRegistro)
-  updateTable();
-  console.log(registros)
+const newRegistro = new Usuario (
+  dataForm.campoNombre.value, 
+  dataForm.campoApellido.value,
+  dataForm.selector.value
+);
+registros.push (newRegistro)
+updateTable();
+console.log(registros)
 }
 
 function updateTable (){
-    registros.forEach(empleado => {
-        const empleadoHTML = document.createElement('tr')
-        empleadoHTML.innerHTML = `
-        <td>${empleado.camponNombre}</td>
-        <td>${empleado.camponApellido}</td>
-        <td>${empleado.selector}</td>`;
-        peopleTable.appendChild(empleadoHTML);
-    });
+const peopleTable = document.querySelector ('#peopleTable');
+const empleadoHTML = document.createElement('tr')
+  registros.map(empleado => {
+      empleadoHTML.innerHTML = `
+      <td>${empleado.nombre}</td>
+      <td>${empleado.apellido}</td>
+      <td>${empleado.cargo}</td>`;
+      peopleTable.appendChild(empleadoHTML);
+  });
 }
