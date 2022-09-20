5eae266c00b8b059111ddbe1773c10f088e1810c
Francisco Villafáñez <110670253+FranVillafanez@users.noreply.github.com>
2022-09-20 16:23:03 -0300

    crud

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Plantilla para crear un usuario
 class Usuario {
-  constructor(nombre, apellido, cargo, mes, horas) {
+  constructor(id, nombre, apellido, cargo, mes, horas) {
+      this.id = id;
       this.nombre = nombre,
       this.apellido = apellido,
       this.cargo = cargo,
@@ -10,12 +11,14 @@ class Usuario {
 }
 // Arreglo para registrar un usuario y fecha
 let registros = []
+
 updateTable();
 const peopleTable = document.querySelector ('#peopleTable tbody');
 const dataForm = document.querySelector ("#addPeople");
 
 function saveData (){
 const newRegistro = new Usuario (
+  dataForm.campoId.value,
   dataForm.campoNombre.value, 
   dataForm.campoApellido.value,
   dataForm.selector.value,
@@ -27,16 +30,42 @@ updateTable();
 console.log(registros)
 }
 
+
 function updateTable (){
 const peopleTable = document.querySelector ('#peopleTable');
 const empleadoHTML = document.createElement('tr')
   registros.map(empleado => {
       empleadoHTML.innerHTML = `
+      <td>${empleado.id}</td>
       <td>${empleado.nombre}</td>
       <td>${empleado.apellido}</td>
       <td>${empleado.cargo}</td>
       <td>${empleado.mes}</td>
-      <td>${empleado.horas}</td>`;
+      <td>${empleado.horas}</td>
+      <td><div><button
+                id="editBtn"
+                type="button"
+                class="btn btn-warning"
+                onclick="editEmpleado(event)"
+                >
+                Editar
+                </button>
+                <button
+                id="deleteBtn_${empleado.id}"
+                type="button"
+                class="btn btn-danger"
+                onclick="deleteEmpleado(event)">
+                Borrar
+                </button>
+            </div></td>
+        `;
       peopleTable.appendChild(empleadoHTML);
   });
 }
+
+function deleteEmpleado(event){
+  const btn = event.taget;
+  const id = btn.id.split("_")[1];
+  registros = registros.filter ((empleado) => empleado.id != id)
+  updateTable();
+} 
